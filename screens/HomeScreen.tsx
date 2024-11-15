3e2cf066b89a2b65f44caec48b8fbef1ab8a7841
Jessel Sookha <jsookha@varsitycollege.co.za>
2024-11-15 14:03:11 +0200

    first commit

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
-import HomeScreen from './screens/HomeScreen';
-import {styles} from './util/styles';
+import {styles} from '../util/styles';
 
-
-export default function App() {
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
